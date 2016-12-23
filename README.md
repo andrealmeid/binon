365186f5e6a008124499b2e7a9fd9973636c90b2
Tony K <fda.andre@gmail.com>
2016-12-23 01:37:39 -0200

    Create getId function and set some instructions on README about the .id files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Binon
 A friendly Telegram bot to remind my girlfriend to take her pills.
+
+To use it, you need to place on the id directory the following information:
+* Your telegram bot token on bot.id file
+* The ID of the contact you want to send to on gf.id
+* Your ID for debug messenge
+
+Then, just run the ```bot.py``` on some vm/server.
--- bot.py
+++ bot.py
@@ -3,26 +3,24 @@ from datetime import datetime
 import time
 from pprint import pprint
 
-with open ("id/bot.id", "r") as bot_file_id:
-    bot_id = bot_file_id.read().replace('\n', '')
+def getId(file):
+    with open (file, "r") as file_id:
+        return file_id.read().replace('\n', '')
 
-with open ("id/krauss.id", "r") as bot_file_id:
-    krauss_id = bot_file_id.read().replace('\n', '')
-
-with open ("id/tony.id", "r") as bot_file_id:
-    tony_id = bot_file_id.read().replace('\n', '')
+bot_id = getId("id/bot.id")
+me_id = getId("id/me.id")
+gf_id = getId("id/gf.id")
 
 bot = telepot.Bot(bot_id)
 
-# response = bot.getUpdates()
-# pprint(response)
+delay_time = 60*30
 
 while 1:
     current_hour = datetime.now().time().hour
     current_minute = datetime.now().time().minute
 
+    #please notice there is no caution with time precision here
     if current_hour >= 11 and current_minute >= 0:
-        bot.sendMessage(tony_id, 'foi')
-        bot.sendMessage(krauss_id, 'Hey xu, nao esquece de tomar a pilula! :)')
-        print("foi")
-    time.sleep(60*15)
+        bot.sendMessage(me_id, 'enviado')
+        bot.sendMessage(gf_id, 'Hey xu, nao esquece de tomar a pilula! :)')
+    time.sleep(delay_time)
